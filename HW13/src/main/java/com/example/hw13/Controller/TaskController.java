d4be97bf5343bc6a09a3f3b1f56ef09a0275dde3
Hashem Bajabaa <Hashem.bajabaa1@gmail.com>
2023-01-25 09:09:37 +0300

    Adding Projects

---
+++ HW13/src/main/java/com/example/hw13/Controller/TaskController.java
@@ -0,0 +1,55 @@
+package com.example.hw13.Controller;
+
+import com.example.hw13.Pojo.Task;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/task")
+public class TaskController {
+
+    ArrayList<Task> tasks = new ArrayList<Task>();
+    @GetMapping("/get")
+    public ArrayList<Task> displayAll(){
+        return tasks;
+    }
+
+    @PostMapping("/add")
+    public String addTask(@RequestBody Task task){
+        tasks.add(task);
+        return "task added successfully";
+    }
+
+    @PutMapping("update/{index}")
+    public String updateTask(@PathVariable int index, @RequestBody Task task){
+        tasks.set(index, task);
+        return "Updated successfully";
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteTask(@PathVariable int id){
+        tasks.remove(id);
+        return "Deleted successfully";
+    }
+
+    @GetMapping("/find/{title}")
+    public Task findTask(@RequestBody String title){
+        for (Task task: tasks) {
+            if(task.getTitle().equals(title)){
+                return task;
+            }
+        }
+        return null;
+    }
+    @PutMapping("/change/{id}")
+    public String changeStatus(@PathVariable int id, @RequestBody String status){
+        for (Task task: tasks) {
+            if(task.getId().equals(id)){
+                task.setStatus(status);
+                return "Changed successfully";
+            }
+        }
+        return "Not Found";
+    }
+}
